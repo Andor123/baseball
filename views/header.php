e6bb5e9846ef64c44dbe69538649d895e7d60e81
Andor Salamon <salamon.andor@gmail.com>
2024-08-06 14:13:01 +0200

    Update header.php
    Removing unused body tag

--- views/header.php
+++ views/header.php
@@ -8,4 +8,3 @@
     <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css">
     <link rel="stylesheet" href="css/bootstrap.min.css">
 </head>
-<body>
